d5b9713b05e0e0b81cfe571e63adc09c68b72220
JBoas <31774842+JBoas@users.noreply.github.com>
2017-12-11 10:20:03 -0600

    Add files via upload

---
+++ pongscreen.py
@@ -0,0 +1,28 @@
+import pygame, sys
+from pygame.locals import *
+
+fps = 150
+screenheight = 450
+screenwidth = 350
+
+	
+def mainfunc():
+	pygame.init()
+	global DISPLAYSURF
+	
+	fpsclock = pygame.time.Clock()
+	DISPLAYSURF = pygame.display.set_mode((screenwidth,screenheight))
+	pygame.display.set_caption("Pong!")
+	
+	while True:
+		for event in pygame.event.get():
+			if event.type == quit:
+				pygame.quit()
+				sys.exit()
+				
+		fpsclock.tick(fps)
+		pygame.display.update()
+
+		
+if __name__=="__main__":
+	mainfunc()
